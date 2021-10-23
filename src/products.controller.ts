4ba7eff3de84a0e7824ae805b9917f37d6b5a191
Renato Marques <renato.mark.silva@gmail.com>
2021-10-23 01:25:05 +0000

    Modelando um produto

--- src/products.controller.ts
+++ src/products.controller.ts
@@ -7,31 +7,39 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
+import { Product } from './product.model';
 
 @Controller('products')
 export class ProductsController {
+  products: Product[] = [
+    new Product('LIV01', "Livro TDD E BDD na prática", 29.90),
+    new Product('LIV02', "Iniciando com Fluter", 39.90),
+    new Product('LIV03', "Inteligencia artificial", 29.90),
+  ];
+  
   @Get()
-  getAll(): string {
-    return 'List all products';
+  getAll(): Product[] {
+    return this.products;
   }
 
   @Get(':id')
-  getById(@Param() params): string {
-    return `return user ${params.id} `;
+  getById(@Param() params): Product {
+    return this.products[0];
   }
 
   @Post()
-  create(@Body() product): string {
-    return `Product created ${product}`;
+  create(@Body() product: Product) {
+    product.id = 100;
+    this.products.push(product);
   }
 
   @Put()
-  update(@Body() product): string {
-    return `Product update ${product}`;
+  update(@Body() product: Product): Product {
+    return product;
   }
 
   @Delete(':id')
-  exclude(@Param() params): string {
-    return `Delete product ${params.id}`;
+  exclude(@Param() params) {
+    return this.products.pop();
   }
 }
